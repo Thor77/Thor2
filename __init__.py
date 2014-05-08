b74779c5eb89bd57afc699a3f0c0b9b5937f4df7
Thor77 <xxthor77xx@gmail.com>
2014-05-08 17:46:53 +0200

    changed nick to Thor2

--- __init__.py
+++ __init__.py
@@ -3,7 +3,7 @@ from base import IRCBot
 bot = IRCBot()
 bot.setServer('irc.quakenet.org')
 bot.setPort(6667)
-bot.setRealName('Thor22')
+bot.setRealName('Thor2')
 bot.setNick('Thor22')
 bot.setCall('$')
 bot.setChannel('#thorsraum')
