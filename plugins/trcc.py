d0c775813d0707ef7c4c3e60a6d4f219850cf594
Thor77 <xxthor77xx@gmail.com>
2014-05-10 14:36:06 +0200

    improvements in the trcc-plugin

--- plugins/trcc.py
+++ plugins/trcc.py
@@ -11,20 +11,28 @@ class TheRadioCC(Plugin):
 		self.addCommand('eannounce', self.eannounce_func, 'enable NP-Announcing')
 		self.addCommand('dannounce', self.dannounce_func, 'disable NP-Announcing')
 		# start timer
-		t = threading.Timer(5, self.announce)
+		t = threading.Timer(10, self.announce)
 		t.daemon = True
 		t.start()
 		self.debug('Timer started!', 2)
 
 	def eannounce_func(self, sender, args):
-		self.np_announce = True
-		self.sendNotice('NP-Announcing enabled!', sender)
+		if not self.np_announce:
+			self.np_announce = True
+			self.sendNotice('NP-Announcing enabled!', sender)
+			self.startNextTimer()
+		else:
+			self.sendNotice('NP-Announcing already enabled!', sender)
 
 	def dannounce_func(self, sender, args):
-		self.np_announce = False
-		self.sendNotice('NP-Announcing disabled!', sender)
+		if self.np_announce:
+			self.np_announce = False
+			self.sendNotice('NP-Announcing disabled!', sender)
+		else:
+			self.sendNotice('NP-Announcing already disabled!', sender)
 
 	def announce(self):
+		# get current song
 		current = requests.get('http://theradio.cc:12011').text
 		# check if changed
 		if self.curr_song != current:
@@ -34,5 +42,9 @@ class TheRadioCC(Plugin):
 				# announcte
 				self.debug('Announcing...', 1)
 				self.sendMessage(current)
-		threading.Timer(10, self.announce).start()
+				self.startNextTimer()
+
+	def startNextTimer(self):
+		timer = threading.Timer(10, self.announce)
+		timer.start()
 		self.debug('Next timer started!', 2)
