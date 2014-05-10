23105adcf31cd74ef18b669f1d207d2a27bd6a74
Thor77 <xxthor77xx@gmail.com>
2014-05-10 20:46:11 +0200

    improved message-plugin

--- plugins/message.py
+++ plugins/message.py
@@ -9,17 +9,17 @@ class Message(Plugin):
         self.addCommand('addMessage', self.addMessage_func, 'addMessage <nick> <msg> | if <nick> joins the channel, he/she will get <message>')
         # events
         self.registerEvent('onUserJoin', self.onUserJoin)
-        #self.registerEvent('onUserMessage', self.onUserMessage)
+        self.registerEvent('onUserMessage', self.onUserMessage)
 
     def onUserJoin(self, eventobj):
         joiner = eventobj.getUser()
         if joiner in self.messages:
-            self.sendMessage('Hallo %s, es wurden Nachrichten für dich hinterlassen:' % joiner)
-            for d in self.messages[joiner]:
-                for sender in d:
-                    msg = d[sender]
-                    self.sendMessage('[{color}08{sender}{color}] => {color}07{nachricht}{color}'.format(color=self.color_code, sender=sender, nachricht=msg))
-            del self.messages[joiner]
+            self.sendWaitingMessages(joiner)
+
+    def onUserMessage(self, eventobj):
+        nick = eventobj.getSender()
+        if nick in self.messages:
+            self.sendWaitingMessages(nick)
 
     def addMessage_func(self, sender, args):
         nick = args[0]
@@ -31,3 +31,11 @@ class Message(Plugin):
         self.sendNotice('Message successfully added!', sender)
         self.sendNotice('Message: %s' % msg, sender)
         self.sendNotice('Nick: %s' % nick, sender)
+
+    def sendWaitingMessages(self, nick):
+        self.sendMessage('Hallo %s, es wurden Nachrichten für dich hinterlassen:' % nick)
+        for d in self.messages[nick]:
+            for sender in d:
+                msg = d[sender]
+                self.sendMessage('[{color}08{sender}{color}] => {color}07{nachricht}{color}'.format(color=self.color_code, sender=sender, nachricht=msg))
+        del self.messages[nick]
