ba25e5620a9a11df2311478f0a4b4c0e3d2ab12e
Thor77 <xxthor77xx@gmail.com>
2014-05-12 18:11:29 +0200

    edited

--- editdatabase.py
+++ editdatabase.py
@@ -4,9 +4,9 @@ import sys
 # args
 if len(sys.argv) == 4:
 
-    dbfile = sys.argv[1]
-    nick = sys.argv[2]
-    newlvl = sys.argv[3]
+    dbfile = str(sys.argv[1])
+    nick = str(sys.argv[2])
+    newlvl = int(sys.argv[3])
 
     print('DatabaseFile: %s' % dbfile)
     print('Nick: %s' % nick)
