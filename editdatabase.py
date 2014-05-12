7a265254d9a74ba1ac5b7fd1f3cd4f86f8d5f134
Thor77 <xxthor77xx@gmail.com>
2014-05-12 17:59:07 +0200

    added databaseedit

---
+++ editdatabase.py
@@ -0,0 +1,31 @@
+import sqlite3
+import sys
+    
+# args
+if len(sys.argv) == 4:
+
+    dbfile = sys.argv[1]
+    nick = sys.argv[2]
+    newlvl = sys.argv[3]
+
+    print('DatabaseFile: %s' % dbfile)
+    print('Nick: %s' % nick)
+    print('NewLevel: %s' % newlvl)
+
+    while True:
+        resume = str('Continue? [y/n]')
+        if resume == 'y':
+            db = sqlite3.connect(dbfile)
+            dbcursor = db.cursor()
+            dbcursor.execute('UPDATE users SET lvl=? WHERE nick=?', (newlvl, nick))
+            print('Success!')
+            break
+        elif resume == 'n':
+            print('Aborted!')
+            break
+
+else:
+    print('To less args!')
+    print('editdatabase.py <databasefile> <nick> <newlvl>')
+
+print('----EXIT---')
