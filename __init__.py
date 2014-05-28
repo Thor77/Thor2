2187dcfaf7ba1dfafccc56c7228a45450e2ea050
Thor77 <xxthor77xx@gmail.com>
2014-05-28 18:22:44 +0200

    created config-file

--- __init__.py
+++ __init__.py
@@ -1,11 +1,23 @@
 from base import IRCBot
+import configparser
+
+config = configparser.ConfigParser()
+config.read('bot.cfg')
+
+server = config.get('Server', 'ip')
+port = config.getint('Server', 'port')
+realname = config.get('IRC', 'realname')
+nick = config.get('IRC', 'nick')
+call = config.get('Bot', 'call')
+channel = config.get('Bot', 'channel')
+debuglvl = config.getint('Bot', 'debuglevel')
 
 bot = IRCBot()
-bot.setServer('irc.theradio.cc')
-bot.setPort(6667)
-bot.setRealName('Thor2')
-bot.setNick('Thor2')
-bot.setCall('$')
-bot.setChannel('#kindergarten')
-bot.setDebugLevel(0)
+bot.setServer(server)
+bot.setPort(port)
+bot.setRealName(realname)
+bot.setNick(nick)
+bot.setCall(call)
+bot.setChannel(channel)
+bot.setDebugLevel(debuglvl)
 bot.start()
